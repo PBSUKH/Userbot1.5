b82ded6b2ad5e2f4aefc01f323b21d0f35ead827
badmunda98 <152181482+badmunda98@users.noreply.github.com>
2024-01-31 13:38:01 +0530

    Update config.py

--- config.py
+++ config.py
@@ -18,7 +18,7 @@ ALIVE_TEXT = getenv("ALIVE_TEXT")
 PM_LOGGER = getenv("PM_LOGGER")
 LOG_GROUP = getenv("LOG_GROUP")
 GIT_TOKEN = getenv("GIT_TOKEN") #personal access token
-REPO_URL = getenv("REPO_URL", "https://github.com/ITZ-ZAID/ZAID-USERBOT")
+REPO_URL = getenv("REPO_URL", "https://github.com/badmunda98/Baduserbot3.0")
 BRANCH = getenv("BRANCH", "master") #don't change
  
 STRING_SESSION1 = getenv("STRING_SESSION1", "")
